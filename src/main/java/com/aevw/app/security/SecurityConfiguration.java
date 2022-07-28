d6f5dab483bfd10c06250c8132034dfccf1fbdc7
Enrique Moran <enrique.morango@udlap.mx>
2022-07-28 10:11:24 -0500

    "New Security Configuration"

--- src/main/java/com/aevw/app/security/SecurityConfiguration.java
+++ src/main/java/com/aevw/app/security/SecurityConfiguration.java
@@ -3,12 +3,8 @@ package com.aevw.app.security;
 import com.aevw.app.entity.AppUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
---
+++ src/test/java/com/aevw/app/service/transaction/TransactionServiceImplementationGetDatesMethodTest.java
@@ -0,0 +1,248 @@
+package com.aevw.app.service.transaction;
+
+import com.aevw.app.exception.ApiRequestException;
+import com.aevw.app.service.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+
+class TransactionServiceImplementationGetDatesMethodTest {
+
+    @Autowired
+    UserService userService;
+
+    TransactionServiceImplementation transactionServiceImplementation = new TransactionServiceImplementation();
+
+    @Test
+    void getDatesNotNull(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+
+        Assertions.assertNotNull(testDates);
+
+    }
+
+    @Test
+    void getDatesArrayEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+
+        Integer expectedStartYear = 2021;
+        Integer expectedStartMonth = 4;
+        Integer expectedStartDay = 12;
+
+        Integer expectedEndYear = 2022;
+        Integer expectedEndMonth = 8;
+        Integer expectedEndDay = 31;
+
+        ArrayList<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(expectedStartYear);
+        expectedArray.add(expectedStartMonth);
+        expectedArray.add(expectedStartDay);
+        expectedArray.add(expectedEndYear);
+        expectedArray.add(expectedEndMonth);
+        expectedArray.add(expectedEndDay);
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+
+        Assertions.assertEquals(testDates,expectedArray);
+
+    }
+
+    @Test
+    void getDatesStartYearEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedStartYear = 2021;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+
+        Assertions.assertEquals(testDates.get(0),expectedStartYear);
+
+    }
+
+    @Test
+    void getDatesStartMonthEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedStartMonth = 4;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+
+        Assertions.assertEquals(testDates.get(1),expectedStartMonth);
+
+    }
+
+    @Test
+    void getDatesStartDayEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedStartDay = 12;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+        Assertions.assertEquals(testDates.get(2),expectedStartDay);
+
+    }
+
+    @Test
+    void getDatesEndYearEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedEndYear = 2022;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+        Assertions.assertEquals(testDates.get(3),expectedEndYear);
+
+    }
+
+    @Test
+    void getDatesEndMonthEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedEndMonth = 8;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+
+        Assertions.assertEquals(testDates.get(4),expectedEndMonth);
+
+    }
+
+    @Test
+    void getDatesEndDayEquals(){
+
+        String rawStartDate = "2021-04-12";
+        String rawEndDate = "2022-08-31";
+        Integer expectedEndDay = 31;
+
+        ArrayList<Integer> testDates = transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+        Assertions.assertEquals(testDates.get(5),expectedEndDay);
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_PassingEmptyStrings(){
+
+        String rawStartDate = "";
+        String rawEndDate = "";
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_PassingNull(){
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(null,null);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_PassingStringWithExtraSlashes(){
+
+        String rawStartDate = "2021-04--12";
+        String rawEndDate = "2022-08-31";
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(null,null);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_PassingStringWithoutSlashes(){
+
+        String rawStartDate = "20210412";
+        String rawEndDate = "20220831";
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(null,null);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_OnlyPassingYears(){
+
+        String rawStartDate = "2021";
+        String rawEndDate = "2022";
+
+        Exception exception = Assertions.assertThrows(
+                    ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+                    }
+                );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_OnlyPassingMonths(){
+
+        String rawStartDate = "05";
+        String rawEndDate = "02";
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void getDatesThrowsAPIRequestException_InvalidData_OnlyPassingYearsAndMonths(){
+
+        String rawStartDate = "2021-05";
+        String rawEndDate = "2022-02";
+
+        Exception exception = Assertions.assertThrows(
+                ApiRequestException.class,()->{
+                    transactionServiceImplementation.getDates(rawStartDate,rawEndDate);
+                }
+        );
+        String expectedMessage = "Invalid data, try again!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
