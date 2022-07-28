52aadc33e710983aae9e43af21d78ed48ed7577d
Enrique Moran <enrique.morango@udlap.mx>
2022-07-28 00:10:35 -0500

    "Changed security configurer for filterchain"

--- src/main/java/com/aevw/app/security/SecurityConfiguration.java
+++ src/main/java/com/aevw/app/security/SecurityConfiguration.java
@@ -1,20 +1,24 @@
 package com.aevw.app.security;
 
+import com.aevw.app.entity.AppUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll().and()
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/p/**","/**").permitAll().and()
                 .authorizeRequests().antMatchers("/console/**").permitAll();
         http.csrf().disable();
         http.headers().frameOptions().disable();
@@ -22,13 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().anyRequest().permitAll();
-
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
-        return super.authenticationManagerBean();
+        return http.build();
     }
 
 }
