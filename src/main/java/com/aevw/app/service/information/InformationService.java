b26ea594c1f79ed79ed2932875cff629dbdf6dd1
Enrique Moran <enrique.morango@udlap.mx>
2022-08-01 11:54:47 -0500

    "Added verified user as parameter"

--- src/main/java/com/aevw/app/service/information/InformationService.java
+++ src/main/java/com/aevw/app/service/information/InformationService.java
@@ -1,13 +1,14 @@
 package com.aevw.app.service.information;
 
 import com.aevw.app.api.response.APIResponse;
+import com.aevw.app.entity.AppUser;
 import com.aevw.app.entity.dto.information.InformationInputDTO;
 
 public interface InformationService {
 
-    APIResponse balance(String token, InformationInputDTO balance);
+    APIResponse balance(String token, InformationInputDTO balance, AppUser balanceUser);
 
-    APIResponse summary(String token, InformationInputDTO summary);
+    APIResponse summary(String token, InformationInputDTO summary, AppUser summaryUser);
 
-    APIResponse series(String token, InformationInputDTO series);
+    APIResponse series(String token, InformationInputDTO series, AppUser seriesUser);
 }
