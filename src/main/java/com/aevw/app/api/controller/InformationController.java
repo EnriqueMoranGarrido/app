cf2a3186fbedbd048c9f8e2bb3caf9b1c5cba904
Enrique Moran <enrique.morango@udlap.mx>
2022-08-01 11:53:15 -0500

    "Added token verification inside the controller"

--- src/main/java/com/aevw/app/api/controller/InformationController.java
+++ src/main/java/com/aevw/app/api/controller/InformationController.java
@@ -1,19 +1,31 @@
 package com.aevw.app.api.controller;
 
 import com.aevw.app.api.response.APIResponse;
+import com.aevw.app.entity.AppUser;
 import com.aevw.app.entity.dto.information.InformationInputDTO;
+import com.aevw.app.repository.UserRepository;
+import com.aevw.app.repository.UserTokenRepository;
 import com.aevw.app.service.information.InformationService;
+import com.aevw.app.utils.TokenVerifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/information")
 public class InformationController {
 
+    private final UserTokenRepository userTokenRepository;
+
+    private final UserRepository userRepository;
+
     private final InformationService informationService;
 
-    public InformationController(InformationService informationService) {
+    public InformationController(UserTokenRepository userTokenRepository, UserRepository userRepository, InformationService informationService) {
+        this.userTokenRepository = userTokenRepository;
+        this.userRepository = userRepository;
         this.informationService = informationService;
     }
 
@@ -22,18 +34,33 @@ public class InformationController {
     public ResponseEntity<Object> getBalance(@RequestHeader(value = "Authorization", defaultValue = "") String auth,
                                              @RequestBody InformationInputDTO balance){
 
-        APIResponse apiResponse = informationService.balance(auth,balance);
+        TokenVerifier tokenVerifier = new TokenVerifier(userTokenRepository, userRepository);
 
-        return ResponseEntity.status(HttpStatus.OK).body(apiResponse.getData());
+        Optional<AppUser> verifyTokenAndGetUser = tokenVerifier.verifyToken(auth);
+
+        if(verifyTokenAndGetUser.isPresent()){
+            APIResponse apiResponse = informationService.balance(auth,balance, verifyTokenAndGetUser.get());
+            return ResponseEntity.status(HttpStatus.OK).body(apiResponse.getData());
+        }
+
+        return null;
     }
 
     @GetMapping("/summary")
     public ResponseEntity<Object> getSummary(@RequestHeader(value = "Authorization", defaultValue = "") String auth,
                                              @RequestBody InformationInputDTO summary){
 
-        APIResponse apiResponse = informationService.summary(auth,summary);
 
-        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+        TokenVerifier tokenVerifier = new TokenVerifier(userTokenRepository, userRepository);
+
+        Optional<AppUser> verifyTokenAndGetUser = tokenVerifier.verifyToken(auth);
+
+        if(verifyTokenAndGetUser.isPresent()){
+            APIResponse apiResponse = informationService.summary(auth,summary,verifyTokenAndGetUser.get());
+
+            return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+        }
+        return null;
     }
 
     @GetMapping("/series")
