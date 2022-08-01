b941707f87ca0ca1f8b764780a3550eedcd26e0f
Enrique Moran <enrique.morango@udlap.mx>
2022-07-31 22:51:01 -0500

    "Created a token verifier class"

---
+++ src/main/java/com/aevw/app/utils/TokenVerifier.java
@@ -0,0 +1,52 @@
+package com.aevw.app.utils;
+
+import com.aevw.app.entity.AppUser;
+import com.aevw.app.entity.UserToken;
+import com.aevw.app.exception.ApiRequestException;
+import com.aevw.app.repository.UserRepository;
+import com.aevw.app.repository.UserTokenRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+public class TokenVerifier {
+
+    private final UserTokenRepository userTokenRepository;
+    private final UserRepository userRepository;
+
+    public TokenVerifier(UserTokenRepository userTokenRepository, UserRepository userRepository) {
+        this.userTokenRepository = userTokenRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public Optional<AppUser> verifyToken(String token){
+
+        UserToken myUserToken = userTokenRepository.findByToken(token);
+
+        ArrayList<Object> myReturnArray = new ArrayList<>();
+
+        if(myUserToken ==null){
+            throw new ApiRequestException("Token not found, try again");
+        }
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret"); //use more secure key
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(myUserToken.getUserEmail())
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+
+            return Optional.ofNullable(userRepository.findByEmail(myUserToken.getUserEmail()));
+
+        } catch (JWTVerificationException exception){
+            //Invalid signature/claims
+            throw new ApiRequestException("Token not found, try again");
+        }
+    }
+}
