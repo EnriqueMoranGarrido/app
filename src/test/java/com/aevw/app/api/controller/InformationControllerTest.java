a6f24d53d20601a7ceb4bb156335368c1ad7ee2f
Enrique Moran <enrique.morango@udlap.mx>
2022-08-04 17:55:49 -0500

    "Created information controller tests file"

---
+++ src/test/java/com/aevw/app/api/controller/InformationControllerTest.java
@@ -0,0 +1,20 @@
+package com.aevw.app.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InformationControllerTest {
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void getSummary() {
+    }
+
+    @Test
+    void getSeries() {
+    }
+}
