77c860180b8bdc3b0a6c825bedcf854b1225264e
Enrique Moran <enrique.morango@udlap.mx>
2022-08-03 20:04:37 -0500

    "Added cache"

--- src/main/java/com/aevw/app/utils/CurrencyConverter.java
+++ src/main/java/com/aevw/app/utils/CurrencyConverter.java
@@ -1,21 +1,49 @@
 package com.aevw.app.utils;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.collect.Maps;
+
+
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
-import javax.money.NumberValue;
 import javax.money.convert.CurrencyConversion;
 import javax.money.convert.MonetaryConversions;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class CurrencyConverter {
 
     public CurrencyConverter() {
     }
 
-    public NumberValue getMonetaryValue(String currency, Double userMoney){
-        MonetaryAmount money = Monetary.getDefaultAmountFactory().setCurrency("USD").setNumber(userMoney).create();
+    private final HashMap<String,Double> currencyMap = Maps.newHashMap();
+    MonetaryAmount money = Monetary.getDefaultAmountFactory().setCurrency("USD").setNumber(1).create();
+
+    public Double getMonetaryValue(String currency, Double userMoney){
+
+        CacheLoader<String, String> loader;
+        loader = new CacheLoader<>() {
+            @Override
+            public String load(String key) {
+                return key.toUpperCase();
+            }
+        };
+
+
+        Cache cache;
+        cache = CacheBuilder.newBuilder().expireAfterAccess(2,TimeUnit.MILLISECONDS)
+                .build(loader);
+
+        cache.getUnchecked(currency);
+
+        if (currencyMap.containsKey(currency)) {
+            return currencyMap.get(currency) * userMoney;
+        }
         CurrencyConversion conversion = MonetaryConversions.getConversion(currency);
-
-        return money.with(conversion).getNumber();
-
+        Double numberValue = money.with(conversion).getNumber().doubleValue();
+        currencyMap.put(currency,numberValue);
+        return numberValue*userMoney;
     }
 }
