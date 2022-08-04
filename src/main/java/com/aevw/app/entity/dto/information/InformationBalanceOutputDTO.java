8cf0a149872f72e148f44017e43bd24d2c8250c6
Enrique Moran <enrique.morango@udlap.mx>
2022-08-03 20:05:14 -0500

    "Used lombok for constructors and data"

--- src/main/java/com/aevw/app/entity/dto/information/InformationBalanceOutputDTO.java
+++ src/main/java/com/aevw/app/entity/dto/information/InformationBalanceOutputDTO.java
@@ -1,23 +1,14 @@
 package com.aevw.app.entity.dto.information;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.money.NumberValue;
 
+@AllArgsConstructor @NoArgsConstructor @Data
 public class InformationBalanceOutputDTO {
 
-    NumberValue balance;
+    Double balance;
 
-    public InformationBalanceOutputDTO() {
-    }
-
-    public InformationBalanceOutputDTO(NumberValue balance) {
-        this.balance = balance;
-    }
-
-    public NumberValue getBalance() {
-        return balance;
-    }
-
-    public void setBalance(NumberValue balance) {
-        this.balance = balance;
-    }
 }
